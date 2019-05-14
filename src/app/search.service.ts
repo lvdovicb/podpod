125d19bdf19d0ce460782adbc357417bebe37632
Ludovic Benoit <lbenoit@aidodys.com>
2019-05-14 16:31:05 +0200

    jkdfljsd

--- src/app/search.service.ts
+++ src/app/search.service.ts
@@ -24,12 +24,8 @@ export class SearchService {
    let headers: HttpHeaders = new HttpHeaders({
     "X-Mashape-Key": "cviLi6JurYmshmkatN7db3K4XZLAp191szBjsnzEEwrYY5NqUE",
     "Accept" : "application/json"
-    // "Access-Control-Allow-Origin": "http://localhost:4200",
-    // "Access-Control-Allow-Headers": "*"
-
    })  
-      
-    // const apiUrl = "https://listennotes.p.mashape.com/api/v1/search?q=";
+ 
     return this.http.get(`https://listennotes.p.mashape.com/api/v1/search?q=${query}`, {headers});
   };
 }
